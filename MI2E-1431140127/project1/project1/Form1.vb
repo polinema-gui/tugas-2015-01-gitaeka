951dcff6bd66f0f55819ef37e7a4633b08a117fc
gita eka febriana <gitaekaf@gmail.com>
2015-10-07 15:23:19 +0700

    tugasgui

---
+++ MI2E-1431140127/project1/project1/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Red Then
+            Me.BackColor = Color.Blue
+        Else
+            Me.BackColor = Color.Red
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        
+    End Sub
+End Class
